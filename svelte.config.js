371cfe0298d921e93e1ae25b0e4e1ffb61fbdb78
NikKnez <nknezevic07@gmail.com>
2024-04-05 15:09:15 +0200

    deleted:    index.html 	modified:   svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -2,13 +2,9 @@ import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  preprocess: vitePreprocess(),
-
   kit: {
-    adapter: adapter(),
-    paths: {
-      base: process.env.NODE_ENV === 'production' ? '/my-amazingly-awesome-portfolio' : '',
-    }
-  }
+    adapter: adapter()
+  },
+  preprocess: vitePreprocess()
 };
 export default config;
