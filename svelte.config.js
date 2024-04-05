ec701ff614ead1d20ed587ca61297d1f4691f3f4
NikKnez <nknezevic07@gmail.com>
2024-04-05 15:04:57 +0200

    modified:   index.html 	modified:   svelte.config.js

--- svelte.config.js
+++ svelte.config.js
@@ -2,12 +2,13 @@ import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+  preprocess: vitePreprocess(),
+
   kit: {
     adapter: adapter(),
     paths: {
       base: process.env.NODE_ENV === 'production' ? '/my-amazingly-awesome-portfolio' : '',
     }
-  },
-  preprocess: vitePreprocess()
+  }
 };
 export default config;
