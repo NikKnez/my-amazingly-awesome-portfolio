1fb194d581732d1f64cc6d724a6c955cdde28eb8
NikKnez <nknezevic07@gmail.com>
2024-08-07 13:26:07 +0200

    modified:   src/components/Footer.svelte

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -8,7 +8,7 @@
     <div class="flex flex-col gap-4 items-center justify-center">
         <p>
             <b class="pr-2">Resume</b>
-            <a href="https://drive.google.com/file/d/1BfheGb2k15YZjWFnwybJWFXKcMlI1zFj/view?usp=drive_link" target="_blank" class="text-violet-400"
+            <a href="https://drive.google.com/file/d/1ZBTKqa08sDwto-wDM7XRJm2o_ANGlqdV/view?usp=drive_link" target="_blank" class="text-violet-400"
                 >nikola_knezevic<sup class=""
                     ><span class="text-xs scale-75 pl-0.5"
                         ><i
